945bdeb584064feadecec30e24646d3730fc4f4c
Yuriy V. <yura3122@gmail.com>
2025-07-07 18:08:27 +0200

    Update BackupStorage.h

--- BackupStorage.h
+++ BackupStorage.h
@@ -3,15 +3,13 @@
 
 #include "CommonTypes.h"
 
-using namespace std;
-
 //abstract class, inherit from this class to implement custom backup storage
 class BackupStorage
 {
 public:
 	BackupStorage() {};
 
-	BackupStorage(string clientId, string volumeId, string region) :
+	BackupStorage(std::string clientId, string volumeId, string region) :
 		m_clientId(clientId),
 		m_volumeId(volumeId),
 		m_region(region)
@@ -20,7 +18,7 @@ public:
 
 	virtual int GetFreeBufferOffsetIndex() = 0;
 
-	virtual void UploadBackupSectorDataAsync(string backupId, string item, string key, const char* bufferOffset, int bufferOffsetIndex, size_t bufferSize) = 0;
+	virtual void UploadBackupSectorDataAsync(std::string backupId, std::string item, std::string key, const char* bufferOffset, int bufferOffsetIndex, size_t bufferSize) = 0;
 
 	virtual void WaitForAllUploadTasksToComplete() = 0;
 
@@ -34,14 +32,14 @@ public:
 
 	virtual RestoreTaskMetaData GetRestoreTaskMetaData(string restoreId) = 0;
 
-	virtual int GetBackupBlockData(string backupId, int partId, string key, const vector<int>& indices, char* buffer) = 0;
+	virtual int GetBackupBlockData(std::string backupId, int partId, std::string key, const std::vector<int>& indices, char* buffer) = 0;
 
-	virtual int ListObjects(string backupId, int partId, vector<int>& objects) = 0;
+	virtual int ListObjects(std::string backupId, int partId, std::vector<int>& objects) = 0;
 
 protected:
-	string m_clientId;
-	string m_volumeId;
-	string m_region;
+	std::string m_clientId;
+	std::string m_volumeId;
+	std::string m_region;
 };
 
 #endif
