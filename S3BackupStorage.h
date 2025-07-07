2e199d993294e918541743fd5abc83732f15ff17
Yuriy V. <yura3122@gmail.com>
2025-07-07 18:11:34 +0200

    Update S3BackupStorage.h

--- S3BackupStorage.h
+++ S3BackupStorage.h
@@ -11,14 +11,10 @@
 #include "core/thread_safe_queue.h"
 #include "BackupStorage.h"
 
-using namespace Aws;
-using namespace S3;
-using namespace S3::Model;
-
 class S3BackupStorage : public BackupStorage
 {
 public:
-	S3BackupStorage(string clientId, string volumeId, string region);
+	S3BackupStorage(std::string clientId, std::string volumeId, std::string region);
 
 	S3BackupStorage() = delete;
 	S3BackupStorage(const S3BackupStorage&) = delete;
@@ -30,31 +26,31 @@ public:
 
 	int GetFreeBufferOffsetIndex() override;
 
-	void UploadBackupSectorDataAsync(string backupId, string item, string key, const char* bufferOffset, int bufferOffsetIndex, size_t bufferSize) override;
+	void UploadBackupSectorDataAsync(std::string backupId, std::string item, std::string key, const char* bufferOffset, int bufferOffsetIndex, size_t bufferSize) override;
 
 	void WaitForAllUploadTasksToComplete() override;
 
-	void UploadBackupMetaData(string backupId, BackupMetaData &metadata) override;
+	void UploadBackupMetaData(std::string backupId, BackupMetaData &metadata) override;
 
-	VolumeMetaData GetVolumeMetaData(string volumeId) override;
+	VolumeMetaData GetVolumeMetaData(std::string volumeId) override;
 
-	BackupMetaData GetBackupMetaData(string backupId) override;
+	BackupMetaData GetBackupMetaData(std::string backupId) override;
 
 	void UploadRestoreTaskMetaData(const RestoreTaskMetaData &metadata) override;
 
-	RestoreTaskMetaData GetRestoreTaskMetaData(string restoreId) override;
+	RestoreTaskMetaData GetRestoreTaskMetaData(std::string restoreId) override;
 
-	int GetBackupBlockData(string backupId, int partId, string key, const vector<int>& indices, char* buffer) override;
+	int GetBackupBlockData(std::string backupId, int partId, std::string key, const std::vector<int>& indices, char* buffer) override;
 
-	int ListObjects(string backupId, int partId, vector<int>& objects) override;
+	int ListObjects(std::string backupId, int partId, std::vector<int>& objects) override;
 
 private:
-	string GetVolumeBucket() const;
+	std::string GetVolumeBucket() const;
 
 	long m_connectTimeoutMs;
 	long m_requestTimeoutMs;
 
-	SDKOptions m_options;
+	Aws::SDKOptions m_options;
 
-	S3Client *m_s3Client;
+	Aws::S3::S3Client *m_s3Client;
 };
